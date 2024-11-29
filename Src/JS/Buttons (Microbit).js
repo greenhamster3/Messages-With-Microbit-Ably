66e5a6388c4ed5d87cb085876f9e7a3c9666a446
Art Of Ink <83496858+greenhamster3@users.noreply.github.com>
2024-11-29 07:03:58 +0000

    Update Buttons (Microbit).js

--- Src/JS/Buttons (Microbit).js
+++ Src/JS/Buttons (Microbit).js
@@ -8,14 +8,14 @@ input.onButtonPressed(Button.B, function () {
     ESP8266_IoT.publishMqttMessage("B", "Buttons", ESP8266_IoT.QosList.Qos0)
 })
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
-ESP8266_IoT.connectWifi("sunnyside", "corrinaanddave081211")
+ESP8266_IoT.connectWifi("WIFI", "PASSWORD)
 basic.forever(function () {
     if (!(ESP8266_IoT.isMqttBrokerConnected())) {
         ESP8266_IoT.setMQTT(
         ESP8266_IoT.SchemeList.TLS,
         "Micro",
-        "v2GmQQ.p5VB0w",
-        "fwTkiXMWWjt4PZaEBzpPJaGawVvMyWZJhuxSWFFxsSk",
+        "UPTO :  IN THE API KEY",
+        "AFTER :",
         "mqtt"
         )
         ESP8266_IoT.connectMQTT("mqtt.ably.io", 8883, false)
